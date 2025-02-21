1688d37f403aebedcec1815ae24c9fdccf90affe
rifat <rifat.simoom@softrobotics.com.bd>
2025-02-22 02:11:16 +0600

    Initial Commit

---
+++ internal/trainer/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/ThreeDotsLabs/wild-workouts-go-ddd-example/internal/common/genproto/trainer"
+	"github.com/ThreeDotsLabs/wild-workouts-go-ddd-example/internal/common/logs"
+	"github.com/ThreeDotsLabs/wild-workouts-go-ddd-example/internal/common/server"
+	"github.com/ThreeDotsLabs/wild-workouts-go-ddd-example/internal/trainer/ports"
+	"github.com/ThreeDotsLabs/wild-workouts-go-ddd-example/internal/trainer/service"
+	"github.com/go-chi/chi/v5"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	logs.Init()
+	err := godotenv.Load("trainer.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	ctx := context.Background()
+
+	application := service.NewApplication(ctx)
+
+	serverType := strings.ToLower(os.Getenv("SERVER_TO_RUN"))
+	switch serverType {
+	case "http":
+		go loadFixtures(application)
+		server.RunHTTPServer(func(router chi.Router) http.Handler {
+			return ports.HandlerFromMux(
+				ports.NewHttpServer(application),
+				router,
+			)
+		})
+	case "grpc":
+		server.RunGRPCServer(func(server *grpc.Server) {
+			svc := ports.NewGrpcServer(application)
+			trainer.RegisterTrainerServiceServer(server, svc)
+		})
+
+	default:
+		panic(fmt.Sprintf("server type '%s' is not supported", serverType))
+	}
+}
---
+++ scripts/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+cd /var/www/html/repos/wild-workouts-go-ddd-example/
+firebase emulators:start --only firestore &
+echo "Waiting for Firestore to start..."
+while ! nc -z localhost 8787; do
+    sleep 2
+done
